aa4ab2782dc57de833e497236a85a2f8de386c47
dritchey01 <david.ritchey@ku.edu>
2023-10-18 18:33:03 -0400

    fix format

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import { ThemeContext } from "./Context/ThemeContext";
 
+
+
 const App = () => {
   const [theme, setTheme] = useState("dark");
   useEffect(() => {
     setTheme(localStorage.getItem("theme") || "dark");
   }, []);
+  
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div
